66875e5e797d177049af9baa6c48a0920a5276cd
TEERAWAT PRAROM <teewat@yahoo.com>
2022-02-08 18:58:05 +1100

    will paginate gem added, pagination for the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
     def home
-        @notes = Note.published.order('updated_at desc')
+        @notes = Note.published.order('updated_at desc').paginate(page: params[:page], per_page: 2)
     end
 end
