b72baa881c3da11786ee16e401c15817a9327a51
TEERAWAT PRAROM <teewat@yahoo.com>
2022-02-11 00:00:24 +1100

    added: home page filtered to display only note's wing and all area wings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
+
     def home
-        @notes = Note.published.order('updated_at desc').paginate(page: params[:page], per_page: 5)
+        ## filter wing note and hard code main of id 1 = all wings
+        @notes = Note.published.joins(:wings)
+        .where("wings.id = #{current_wing.id} or wings.id = 1")
+        .order('updated_at desc')
+        .paginate(page: params[:page], per_page: 5)
     end
+        
+
 end
